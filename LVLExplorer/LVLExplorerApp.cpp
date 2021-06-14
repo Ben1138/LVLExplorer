98abdc77107550ae2d1c54b0f4cfcd091210bf5d
Ben1138 <marten.mildt@web.de>
2021-06-14 16:47:43 +0200

    Upgraded to latest lib. Added progress bar dialog when loading.

--- LVLExplorer/LVLExplorerApp.cpp
+++ LVLExplorer/LVLExplorerApp.cpp
@@ -2,24 +2,13 @@
 
 wxIMPLEMENT_APP(LVLExplorerApp);
 
-using LibSWBF2::ELogType;
-using LibSWBF2::Logging::Logger;
-using LibSWBF2::Logging::LogCallback;
-
 
 LVLExplorerFrame* LVLExplorerApp::m_frame;
 
 bool LVLExplorerApp::OnInit()
 {
-	Logger::SetLogfileLevel(ELogType::Warning);
-	Logger::SetLogCallback(&SWBF2LogCallback);
-
 	m_frame = new LVLExplorerFrame();
 	m_frame->Show();
-	return true;
-}
 
-void LVLExplorerApp::SWBF2LogCallback(const LoggerEntry* loggerEntry)
-{
-	m_frame->AddLogLine(loggerEntry->ToString().Buffer());
+	return true;
 }
